05a38c1c48dceedf03853b83207ee56e2d92f59b
M Atheer <masreplay@gmail.com>
2022-04-22 22:11:09 +0300

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import PureTimetable from '@/pure-timetable.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    PureTimetable
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <pure-timetable />
+  </div>
+</template>
---
+++ dist/types/src/entry.esm.d.ts
@@ -0,0 +1,5 @@
+import { PluginObject } from 'vue';
+import component from "./pure-timetable.vue";
+declare type InstallableComponent = typeof component & PluginObject<any>;
+declare const _default: InstallableComponent;
+export default _default;
